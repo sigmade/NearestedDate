94869b0d732e19196eb0d19a56e9482c427987f4
Egor <sychyov1991@mail.ru>
2022-02-25 19:30:03 +0600

    Add project files.

---
+++ NearestedDate/Initializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NearestedDate
+{
+    internal class Initializer
+    {
+        public List<Classifier> Classifiers()
+        {
+            return new List<Classifier>
+            {
+                new () { Id = 1, Date = DateTime.Parse("01.01.2020") },
+                new () { Id = 2, Date = DateTime.Parse("01.01.2021") },
+                new () { Id = 3, Date = DateTime.Parse("01.01.2022") }
+            };
+        }
+
+        public List<InputData> InputData()
+        {
+            return new List<InputData>
+            {
+                new () { Id = 1, Date = DateTime.Parse("02.01.2022") },
+                new () { Id = 2, Date = DateTime.Parse("02.01.2021") },
+                new () { Id = 3, Date = DateTime.Parse("03.01.2020") }
+            };
+        }
+    }
+
+    public class Classifier
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class InputData
+    {
+        public int Id { get; set; } 
+        public DateTime Date { get; set; }
+    }
+
+    public class OutputData
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime ClassifierDate { get; set; }
+    }
+}
---
+++ NearestedDate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace NearestedDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var init = new Initializer();
+
+            //выгружаем отсортировааные даты в лист,
+            //чтобы только один раз в бд обратится
+            var clsDate = init.Classifiers()
+                .OrderByDescending(x => x.Date)
+                .Select(x => x.Date)
+                .ToList();
+
+            var outPut = from input in init.InputData()
+                         select new OutputData
+                         {
+                             Id = input.Id,
+                             Date = input.Date,
+                             ClassifierDate = clsDate
+                                .Where(d => d.Date < input.Date)
+                                .First()
+                         };
+
+            foreach (var i in outPut)
+            {
+                Console.WriteLine($"{i.Id} -- {i.Date} -- {i.ClassifierDate}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
